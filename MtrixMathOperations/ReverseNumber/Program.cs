33e367d56327cc922c256304375c0f476933509e
manideep-bestshop <nmanideep1015@gmail.com>
2024-04-25 21:17:33 +0530

    added fibonacci swapnumber reverse number prograns

---
+++ MtrixMathOperations/ReverseNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, reverse = 0, rem;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("Reversed Number: " + reverse);
+        }
+    }
+}
