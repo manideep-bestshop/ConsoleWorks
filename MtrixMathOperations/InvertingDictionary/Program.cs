d59f91f430e27a89fc5bd32c07ed9a336a37b6c7
manideep-bestshop <nmanideep1015@gmail.com>
2024-05-05 13:20:19 +0530

    added dictionary programs

---
+++ MtrixMathOperations/InvertingDictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertingDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> heroesAbilities = new Dictionary<string, string>
+            {
+                 { "Flash", "Super Speed" },
+                 {  "Green Lantern", "Power Ring" },
+                 { "Aquaman", "Atlantean Strength" }
+            };
+
+            // Inverting the dictionary using LINQ
+            var inverted = heroesAbilities.ToDictionary(x => x.Value, x => x.Key);
+
+            // Inverting the dictionary using a foreach loop
+            Dictionary<string, string> inverted2 = new Dictionary<string, string>();
+            foreach (var item in heroesAbilities)
+            {
+                inverted2[item.Value] = item.Key;
+            }
+        }
+    }
+}
