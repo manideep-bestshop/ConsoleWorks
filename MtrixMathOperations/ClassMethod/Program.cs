01ba8cf707e8a13d3a32fdde4fa2ca7f411ea44d
manideep-bestshop <nmanideep1015@gmail.com>
2024-05-06 22:06:15 +0530

    added Bsic program

---
+++ MtrixMathOperations/ClassMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    class Dog
+    {
+        string breed;
+
+        public void bark()
+        {
+            Console.WriteLine("Bark Bark !!");
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            // create Dog object 
+            Dog bullDog = new Dog();
+
+            // access breed of the Dog 
+            bullDog.breed = "Bull Dog";
+            Console.WriteLine(bullDog.breed);
+
+            // access method of the Dog
+            bullDog.bark();
+
+            Console.ReadLine();
+
+        }
+    }
+}
