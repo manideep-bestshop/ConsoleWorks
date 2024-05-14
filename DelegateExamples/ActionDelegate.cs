01de3cc75c4aed55af553c8dcf0e14c2582a4a62
manideep-bestshop <nmanideep1015@gmail.com>
2024-05-14 14:49:52 +0530

    cust multi action Delegates

---
+++ DelegateExamples/ActionDelegate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExamples
+{
+    internal class ActionDelegate
+    {
+
+        public static void myfun(int p, int q)
+        {
+            Console.WriteLine(p - q);
+        }
+        static void Main()
+        {
+            Action<int, int> val = myfun;
+            val(20, 5);
+        }
+    }
+}
---
+++ MulticastDelegate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MulticastDelegate.rectangle;
+
+namespace MulticastDelegate
+{
+    class rectangle
+    {
+
+        // declaring delegate
+        public delegate void rectDelegate(double height,
+                                          double width);
+
+        public void area(double height, double width)
+        {
+            Console.WriteLine("Area is: "+(width * height));
+        }
+
+        public void perimeter(double height, double width)
+        {
+            Console.WriteLine("Perimeter is:  "+ 2 * (width + height));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle rect = new rectangle();
+
+
+            rectDelegate rectdele = new rectDelegate(rect.area);
+
+
+            rectdele += rect.perimeter;
+
+
+            rectdele(6.3, 4.2);
+            Console.WriteLine();
+
+
+            rectdele(16.3, 10.3);
+
+        }
+    }
+}
