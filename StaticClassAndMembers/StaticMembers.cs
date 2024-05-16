30ffbfcc416ece2e677f1c6958cb128d884c5e80
manideep-bestshop <nmanideep1015@gmail.com>
2024-05-16 18:13:22 +0530

    StaticMembers

---
+++ StaticClassAndMembers/StaticMembers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassAndMembers
+{
+    public class Mobile
+    {
+        public static int Price = 12000;
+
+        public static int Qty
+        {
+            get
+            {
+                return 5;
+            }
+        }
+        public static void TotalAmount()
+        {
+            Console.WriteLine("Total Price :" + (Price * Qty));
+        }
+    }
+    internal class StaticMembers
+    {
+        static void Main()
+        {
+            Console.WriteLine("Mobile Price :"+Mobile.Price);
+
+            Console.WriteLine("Quantity :"+Mobile.Qty);
+            
+            Mobile.TotalAmount();   
+
+        }
+    }
+}
