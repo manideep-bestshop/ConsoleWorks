9185b139f5c8d5a858fe4250b6d9704f3d7ec4d4
manideep-bestshop <nmanideep1015@gmail.com>
2024-05-18 17:18:12 +0530

    BoxingUnBoxing Enums Array

---
+++ BoxingUnBoxing/ConstEnums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnBoxing
+{
+    internal class ConstEnums
+    {
+        public void ConstMethod()
+        {
+            const int a = 233;
+            Console.WriteLine(a);
+            //a = a + 10; CTE
+            int b = a;
+            Console.WriteLine(b);
+        }
+        static void Main()
+        {
+            ConstEnums constEnums = new ConstEnums();
+            constEnums.ConstMethod();
+
+
+        }
+    }
+}
