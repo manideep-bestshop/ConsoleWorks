512ef9d5cb4157aae3e2f0b80a28c6e356f931c0
manideep-bestshop <nmanideep1015@gmail.com>
2024-05-20 17:06:10 +0530

    string string builder

---
+++ StringAndStringBuilder/ClassesVsInheritance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAndStringBuilder
+{
+    class B1
+    {
+        public int i = 19;
+        public void Show()
+        {
+            Console.WriteLine("From show of B1..");
+        }
+    }
+    class D1 : B1
+    {
+        public int j = 28;
+        public void Display()
+        {
+            Console.WriteLine("From display D1..");
+        }
+    }
+    internal class ClassesVsInheritance
+    {
+        static void Main()
+        {
+            D1 d = new D1();
+            B1 b = d; //implicit casting
+
+            Console.WriteLine(b is D1);
+            Console.WriteLine(b.i);
+
+            b.Show();
+        }
+    }
+}
---
+++ StringAndStringBuilder/StringBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAndStringBuilder
+{
+    internal class StringBuilder
+    {
+        static void Main()
+        {
+            string s1 = "Computer";
+            string s2=s1.Substring(3);
+            Console.WriteLine(s2);
+
+            string s3=s1.Substring(3,3);
+            Console.WriteLine(s3);
+            
+
+
+        }
+    }
+}
