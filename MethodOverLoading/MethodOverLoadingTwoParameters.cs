259cd62bf8a50c28264327ac6743d041f4186875
manideep-bestshop <nmanideep1015@gmail.com>
2024-05-22 21:55:21 +0530

    NMethod OverLoading

---
+++ MethodOverLoading/MethodOverLoadingTwoParameters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverLoading
+{
+    class MethodOver
+    {
+        public void Show(int x,byte y)
+        {
+            Console.WriteLine(x+" "+y);
+        }
+        public void Show(byte x,int y)
+        {
+            Console.WriteLine(x+" "+y);
+        }
+    }
+    internal class MethodOverLoadingTwoParameters
+    {
+        static void Main()
+        {
+            int i = 12;
+            byte j = 33;
+            MethodOver m= new MethodOver();
+            m.Show(i,j);
+            byte b = 98;
+            int n = 27;
+            MethodOver m2= new MethodOver();
+            m2.Show(b, n);
+            m2.Show((int)j, b);
+
+        }
+    }
+}
---
+++ MethodOverLoading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverLoading
+{
+    class Demo
+    {
+        public void Show()
+        {
+            Console.WriteLine("From show no parameters");
+        }
+        public void Show(int n)
+        {
+            Console.WriteLine("From show"+ n);
+        }
+        public void Show(string s)
+        {
+            Console.WriteLine("From Show "+s);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo demo = new Demo();
+            demo.Show();
+            demo.Show(12);
+            demo.Show("manideep");
+        }
+
+    }
+}
