b7d5fed41f0dc0e42a4fda037418c7fe253f385a
manideep-bestshop <nmanideep1015@gmail.com>
2024-05-23 13:54:35 +0530

    Exception Handlings

---
+++ ExceptionHandlings/CustomizedExceptions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlings
+{
+    class InvSalException : Exception
+    {
+        private int Salary;
+        public InvSalException(int pay)
+        {
+            Salary = pay;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Invalid Salary :"+Salary);
+        }
+    }
+    internal class CustomizedExceptions
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter Emp Id");
+                int Id = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Salary");
+                int salary = int.Parse(Console.ReadLine());
+
+                if (salary < 0)
+                    throw new InvSalException(salary);
+                Console.WriteLine("Id"+Id);
+                Console.WriteLine("Salary:"+salary);
+
+
+            }
+            catch(InvSalException e)
+            {
+                Console.WriteLine("Salary cant be less than Zero..");
+                e.Show();
+            }
+        }
+    }
+}
---
+++ ExceptionHandlings/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter first value..");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Second Value");
+                int b = int.Parse(Console.ReadLine());
+
+                int c = a / b;
+                Console.WriteLine("Quot is "+c);
+
+                Console.WriteLine("Enter n Value:");
+                int n = int.Parse(Console.ReadLine());
+
+                int[] arr = new int[3];
+                Console.WriteLine("N ="+n);
+
+                Console.WriteLine("Length :"+arr.Length);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Wrong I/P..");
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine("array size can't be negative..");
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("can't divide by zero..");
+            }
+            finally
+            {
+                Console.WriteLine("from finally block..");
+            }
+        }
+    }
+}
