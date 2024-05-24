46204a935a7c47db0c59e54351ea72732c4e9d66
manideep-bestshop <nmanideep1015@gmail.com>
2024-05-24 16:50:18 +0530

    Reflection

---
+++ ReflectionExamples/InfoAboutTypeReflection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExamples
+{
+    class Demo1 { }
+    class Sample { }
+    interface If1 { }
+    enum WeekDays { }
+    interface If2 { }
+    class Test { }
+    struct Student { }
+    struct Employee { }
+    enum Months { }
+
+    internal class InfoAboutTypeReflection
+    {
+        static void Main()
+        {
+            Assembly a=Assembly.GetExecutingAssembly();
+            Type[] t=a.GetTypes();
+            foreach(Type t1 in t)
+            {
+                if(t1.IsClass)
+                    Console.WriteLine(t1+"[Class]");
+                else
+                    if(t1.IsInterface)
+                    Console.WriteLine(t1+"[Interface]");
+                else
+                    if(t1.IsEnum)
+                    Console.WriteLine(t1+"[Enum]");
+                else
+                    Console.WriteLine("[Struct]");
+            }
+        }
+    }
+}
---
+++ ReflectionExamples/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExamples
+{
+    class Demo
+    {
+        public int a = 19;
+        public bool b = true;
+        public double d = 7.3;
+        public int prop1 { get; set; }
+        public int prop2 { get; set; }
+
+        public Demo() { }
+        public Demo(byte i) { }
+        public void Show() { }
+        public void display() { }
+        public void Retr(int n, float m) { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d=new Demo();
+            Type t=d.GetType();
+            Console.WriteLine(t);
+            FieldInfo i = t.GetField("a");
+            Console.WriteLine(i);
+
+            FieldInfo[] fi = t.GetFields();
+
+            foreach(FieldInfo f in fi)
+                Console.WriteLine(f);
+
+            MethodInfo[] mi=t.GetMethods();
+
+            foreach(MethodInfo m in mi)
+            {
+                Console.WriteLine(m);
+                ParameterInfo[] pi= m.GetParameters();
+                foreach(ParameterInfo p in pi)
+                    Console.WriteLine(p);
+            }
+            ConstructorInfo[] ci= t.GetConstructors();
+            foreach(ConstructorInfo c in ci)
+                Console.WriteLine(c);
+            PropertyInfo[] p1 = t.GetProperties();
+            foreach(PropertyInfo p in p1)
+                Console.WriteLine(p);
+
+            Console.WriteLine("===========");
+            TypeInfo ti = t.GetTypeInfo();
+            Console.WriteLine(ti);
+            Console.WriteLine("===========");
+        }
+    }
+}
