b485e57b0f957d1711f63753de7145a58ee59b78
manideep-bestshop <nmanideep1015@gmail.com>
2024-05-25 13:26:44 +0530

    Partial Class methods

---
+++ PartialCalssMethods/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialCalssMethods
+{
+    class Demo
+    {
+        public int k;
+    }
+    partial class Test:Demo
+    {
+        public int i;
+        partial void Show();
+    }
+    partial class Test
+    {
+        public int j;
+        partial void Show()
+        {
+            Console.WriteLine("From partial show method..");
+        }
+        public void display()
+        {
+            Console.WriteLine("From display");
+            Show();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            Console.WriteLine(test.i+" "+test.j+""+test.k);
+            test.display();
+        }
+    }
+}
