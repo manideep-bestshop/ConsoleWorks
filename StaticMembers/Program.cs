e2bf6a6bd3ed57dccb17df13c08b034866d42969
manideep-bestshop <nmanideep1015@gmail.com>
2024-05-27 15:36:47 +0530

    Static members

---
+++ StaticMembers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMembers
+{
+
+    public class Cat
+    {
+
+        public Cat()
+        {
+            instances++;
+        }
+
+        public static void HowManyCats()
+        {
+            Console.WriteLine("{0} cats adopted",
+                  instances);
+        }
+        private static int instances = 0;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat.HowManyCats();
+            Cat frisky = new Cat();
+            Cat.HowManyCats();
+            Cat whiskers = new Cat();
+            Cat.HowManyCats();
+        }
+    }
+}
