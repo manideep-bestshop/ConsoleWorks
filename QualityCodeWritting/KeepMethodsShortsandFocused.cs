e022d49a6c9f1e438f1ea3956ac95d265f96958c
manideep-bestshop <nmanideep1015@gmail.com>
2024-06-03 15:55:26 +0530

    added programs

---
+++ QualityCodeWritting/KeepMethodsShortsandFocused.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualityCodeWritting
+{
+    public class OrderProcessor
+    {
+        public void ProcessOrder(Order order)
+        {
+            if (ValidateOrder(order))
+            {
+                SaveOrder(order);
+                NotifyCustomer(order);
+            }
+        }
+
+        private bool ValidateOrder(Order order)
+        {
+            // Validate order details
+            return order != null && order.Items.Count > 0;
+        }
+
+        private void SaveOrder(Order order)
+        {
+            // Save order to the database
+            Console.WriteLine("Order saved to the database.");
+        }
+
+        private void NotifyCustomer(Order order)
+        {
+            // Send notification to the customer
+            Console.WriteLine("Customer notified.");
+        }
+    }
+    internal class KeepMethodsShortsandFocused
+    {
+        static void Main()
+        {
+            Order order = new Order { Items = new List<string> { "Item1", "Item2" } };
+            OrderProcessor orderProcessor = new OrderProcessor();
+            orderProcessor.ProcessOrder(order);
+        }
+    }
+}
---
+++ QualityCodeWritting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualityCodeWritting
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { FirstName = "Manideep", LastName = "Nampally" };
+            Console.WriteLine(customer.GetFullName());
+        }
+    }
+}
