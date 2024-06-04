620de6fc9ccb95a0a3a003da5e342ae341892f55
manideep-bestshop <nmanideep1015@gmail.com>
2024-06-04 12:27:03 +0530

    string and string builder

---
+++ StringStringBuilder/StringWithLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringStringBuilder
+{
+    internal class StringWithLoop
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 10; i++)
+            {
+                sb.Append(i);
+                sb.Append(" ");
+            }
+
+            Console.WriteLine("Numbers: " + sb.ToString());
+        }
+    }
+}
