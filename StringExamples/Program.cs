73aada611ee17baecd946e5af2b3cebbd78550ac
manideep-bestshop <nmanideep1015@gmail.com>
2024-06-14 16:08:23 +0530

    added programs on string

---
+++ StringExamples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Console.WriteLine("Enter string value");
+            text=Console.ReadLine();
+            Console.WriteLine("Entered String is : "+text);
+        }
+    }
+}
---
+++ StringExamples/StringWithSpace.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExamples
+{
+    internal class StringWithSpace
+    {
+        static void Main()
+        {
+            string value;
+            int length = 0;
+
+            Console.Write("Input the string: ");
+            value = Console.ReadLine();
+
+            Console.Write("The characters of the string are: ");
+
+           
+            while (length <= value.Length - 1)
+            {
+                Console.Write("{0} ", value[length]);
+                length++; 
+            }
+            Console.WriteLine();
+        }
+    }
+}
